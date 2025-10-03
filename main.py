cccead04eed355adde8bb62fbf685fadfed7cd04
noemirtil <mirtil.noe@gmail.com>
2025-10-03 10:00:27 +0200

    main.py created

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from ProyectoLogarse import login
+
+
+def main():
+    print(
+        """
+
+
+=================== Escuela The Corner ================
+
+
+          """
+    )
+
+
+login()
+
+
+if __name__ == "__main__":
+    main()
